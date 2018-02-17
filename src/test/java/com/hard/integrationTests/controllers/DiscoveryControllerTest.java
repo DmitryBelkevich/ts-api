f65005281c2592aaa6207bd5c246f1b4e9197f38
hard <harleydav@mail.ru>
2018-02-17 17:13:07 +0300

    DiscoveryControllerTest

---
+++ src/test/java/com/hard/integrationTests/controllers/DiscoveryControllerTest.java
@@ -0,0 +1,47 @@
+package com.hard.integrationTests.controllers;
+
+import com.hard.config.MvcConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = {
+                MvcConfig.class,
+        }
+)
+@WebAppConfiguration
+public class DiscoveryControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getMessage_Test() throws Exception {
+        mockMvc.perform(
+                get("/api/discovery")
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+        ;
+    }
+}
