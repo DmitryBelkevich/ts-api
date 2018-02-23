0643d93ef0d61b8a6882c15b0ddeed8dd683dc49
hard <harleydav@mail.ru>
2018-02-23 14:03:34 +0300

    DiscoveryController

--- src/main/java/com/hard/controllers/DiscoveryController.java
+++ src/main/java/com/hard/controllers/DiscoveryController.java
@@ -1,6 +1,9 @@
 package com.hard.controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/discovery")
 public class DiscoveryController {
     @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8")
-    public String getMessage() {
-        return "Hello World";
+    public ResponseEntity<String> getMessage() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+
+        String body = "Hello World";
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .headers(headers)
+                .body(body);
     }
 }
--- src/test/java/com/hard/integrationTests/controllers/DiscoveryControllerTest.java
+++ src/test/java/com/hard/integrationTests/controllers/DiscoveryControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,6 +16,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,6 +45,9 @@ public class DiscoveryControllerTest {
                 get("/api/discovery")
         )
                 .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+                .andExpect(header().string(HttpHeaders.CONTENT_LENGTH, "11"))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
                 .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
                 .andExpect(content().string("Hello World"))
         ;
