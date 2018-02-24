3725629558b38b98ae9a601591d761764ee96714
hard <harleydav@mail.ru>
2018-02-25 02:38:18 +0300

    db positions en

--- src/main/resources/database/02.sql
+++ src/main/resources/database/02.sql
@@ -2,12 +2,12 @@ INSERT INTO roles (title) VALUES
   ('ROLE_USER'),
   ('ROLE_ADMIN');
 
-INSERT INTO positions (title_ru) VALUES
-  ('Собственник'),
-  ('Председатель ТС'),
-  ('Бухгалтер'),
-  ('Инженер'),
-  ('Сантехник'),
-  ('Диспетчер'),
-  ('Консьерж'),
-  ('Программист');
+INSERT INTO positions (title_ru, title_en) VALUES
+  ('Собственник', 'Owner'),
+  ('Председатель ТС', 'Chairperson HA'),
+  ('Бухгалтер', 'Accountant'),
+  ('Инженер', 'Engineer'),
+  ('Сантехник', 'Plumber'),
+  ('Диспетчер', 'Dispatcher'),
+  ('Консьерж', 'Concierge'),
+  ('Программист', 'Developer');
